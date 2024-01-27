4840c359111ba9ef736aef38dcdeb2ee0c081795
ReverendSpeed <reverendspeed@gmail.com>
2024-01-27 16:42:23 +0000

    Preparing to integrate audio.

--- Scripts/player01.gd
+++ Scripts/player01.gd
@@ -1,6 +1,6 @@
 extends CharacterBody3D
 
-const move_speed : float = 1.0
+const move_speed : float = 4.0
 const jump_speed : float = 8.0
 
 const gravity : float = 20.0
@@ -13,5 +13,5 @@ func _physics_process(delta):
 	var input = Input.get_vector("p1_move_lft", "p1_move_rgt", "p1_move_fwd", "p1_move_bck")
 	
 	var dir = Vector3 (input.x, 0, input.y)
-	velocity = dir
+	velocity = dir * move_speed
 	move_and_slide()
